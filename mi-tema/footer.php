a836d132f1391899857eda86df0a7de7e84c848e
maryl_hydra <maryl@hydralab.mx>
2017-06-24 07:43:01 -0500

    First commit

---
+++ mi-tema/footer.php
@@ -0,0 +1,12 @@
+    </div> <!-- /.container -->
+
+		<footer class="blog-footer">
+      <p>TechWo Community 2017®</a>.</p>
+      <p>
+        <a href="#">Volver al inicio</a>
+      </p>
+    </footer>
+
+<?php wp_footer(); ?> 
+  </body>
+</html>
